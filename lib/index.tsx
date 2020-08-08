c62d8fbb33f7b50741f588cf6bbde516ed584452
tsai <mengjietsai@163.com>
2020-08-08 23:34:41 +0800

    使用HtmlWebpackPlugin插件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,4 @@
-console.log('hi333')
+console.log('hi1111333')
+const div = document.createElement('div')
+div.innerHTML = 'hi'
+document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   // 开发模式
   mode: 'production',
@@ -24,4 +25,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GULU-UI-React',
+      template: 'index.html',
+    }),
+  ],
 }
