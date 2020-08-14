0e3978d8ae45d641034a1d886fcb9e6807c524c4
tsai <mengjietsai@163.com>
2020-08-14 21:22:45 +0800

    publish成功

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,4 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-
-import Icon from './icon/icon'
-const fn: React.MouseEventHandler = (e)=>{
-    console.log(e.currentTarget)
-}
-ReactDom.render(<React.Fragment>
-    <Icon name={"qq"}
-          onClick={fn}
-          onMouseEnter={()=> console.log('enter')}
-          onMouseLeave={()=> console.log('leave')}
-    />
-</React.Fragment>, document.querySelector("#root"))
+// 引入然后导出
+export {default as Icon} from './icon/icon'
 
 
