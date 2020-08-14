6bc9bf2e7e54db938e07be87a003d004f469eb0d
tsai <mengjietsai@163.com>
2020-08-14 17:46:06 +0800

    enzyme和 enzyme-adapter-react-16的配置 模拟点击事件

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,19 @@
 import renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../icon'
+import {mount} from 'enzyme'
 
 describe('icon', () => {
   it('render successfully', () => {
-    const json = renderer.create(<Icon name={'qq'}/>).toJSON()
+    const json = renderer.create(<Icon name='qq'/>).toJSON()
     expect(json).toMatchSnapshot()
+  })
+  it('onClick', () => {
+    // 模拟click
+    const fn = jest.fn()
+    // 挂在在内存中
+    const component = mount(<Icon name="alipay" onClick={fn} />)
+    component.find('svg').simulate('click')
+    expect(fn).toBeCalled()
   })
 })
