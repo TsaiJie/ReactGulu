b628612a1d58e4d3305e0ff6399a54a8fdea2c75
tsai <mengjietsai@163.com>
2020-08-15 19:10:13 +0800

    安装react-router react-router-dom， 以及基本使用

--- example.tsx
+++ example.tsx
@@ -1,9 +1,36 @@
-import Icon from './lib/icon/icon'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {HashRouter as Router, Link, Route} from 'react-router-dom'
+import IconExample from "./lib/icon/icon.example";
+import ButtonExample from "./lib/button.example";
 
 ReactDOM.render((
+  <Router>
     <div>
-        <Icon name={'alipay'} />
+      <header>
+        <div className="logo">
+          Gulu-React
+        </div>
+      </header>
+      <div>
+        <aside>
+          <h2>组件</h2>
+          <ul>
+            <li>
+              <Link to='/icon'>Icon</Link>
+            </li>
+            <li>
+              <Link to='/button'>Button</Link>
+            </li>
+
+          </ul>
+        </aside>
+      </div>
+      <main>
+        <Route path='/icon' component={IconExample}/>
+        <Route path='/button' component={ButtonExample}/>
+        main
+      </main>
     </div>
+  </Router>
 ), document.querySelector('#root'))
---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Icon from "./icon";
+
+const IconExample: React.FunctionComponent = () => {
+    return (
+        <div>
+            <Icon name={'qq'}/>
+        </div>
+    )
+}
+export default IconExample
