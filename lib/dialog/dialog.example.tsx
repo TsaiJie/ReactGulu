711478128df3f554cf6f3910c7abb6b7e4a6bf5f
tsai <mengjietsai@163.com>
2020-08-16 11:08:38 +0800

    gulu-dialog 添加事件处理

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,13 +1,36 @@
-import React, {useState} from "react";
+import React, {Fragment, useState} from "react";
 import Dialog from './dialog'
 
 const DialogExample: React.FunctionComponent = () => {
   const [x, setX] = useState(false)
-  return (<div>
-    <button onClick={() => setX(!x)}>click</button>
-    <Dialog visible={x}>
-      <div>hi</div>
-    </Dialog>
-  </div>)
+  const [y, setY] = useState(false)
+  return (<Fragment>
+    <div>
+      <h1>example1</h1>
+      <button onClick={() => setX(!x)}>click</button>
+      <Dialog visible={x} buttons={
+        [
+          <button onClick={() => setX(false)}>1</button>,
+          <button onClick={() => setX(false)}>2</button>
+        ]
+        
+      } onClose={() => {setX(false)}}>
+        <div>hi</div>
+      </Dialog>
+    </div>
+    <div>
+      <h1>example2</h1>
+      <button onClick={() => setY(!y)}>click</button>
+      <Dialog visible={y} buttons={
+        [
+          <button onClick={() => setY(false)}>1</button>,
+          <button onClick={() => setY(false)}>2</button>
+        ]
+        
+      } onClose={() => {setY(false)}} clickMaskClose={true}>
+        <div>hi</div>
+      </Dialog>
+    </div>
+  </Fragment>)
 }
 export default DialogExample
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,33 +1,52 @@
-import React, {Fragment} from "react";
+import React, {Fragment, ReactElement} from "react";
 import './dialog.scss'
 import {Icon} from "../index";
 import {scopedClassMaker} from "../classes";
+
 interface Props {
   visible: boolean;
+  buttons: Array<ReactElement>;
+  onClose: React.MouseEventHandler;
+  clickMaskClose?: boolean
 }
 
 const scopedClass = scopedClassMaker('gulu-dialog')
 const sc = scopedClass
 const Dialog: React.FunctionComponent<Props> = (props) => {
-  const {visible, children} = props
+  
+  const {visible, children, buttons, onClose, clickMaskClose} = props
+  const onClickClose: React.MouseEventHandler = (e) => {
+    onClose(e)
+  }
+  const onClickMaskClose: React.MouseEventHandler = (e) =>{
+    clickMaskClose && onClose(e)
+  }
+  
   // 必须返回一个null或者组件children有可能是组件也可能不是组件
   return (
     visible ? <Fragment>
-      <div className={sc("mask")}>
+      <div className={sc("mask")} onClick={onClickMaskClose}>
       </div>
       <div className={sc()}>
-        <div className={sc('close')}>
+        <div className={sc('close')} onClick={onClickClose}>
           <Icon name={'close'}/>
         </div>
         <header className={sc('header')}>提示</header>
         <main className={sc('main')}>{children}</main>
         <footer className={sc('footer')}>
-          <button>OK</button>
-          <button>Cancel</button>
+          {
+            buttons.map((button, index) =>
+              // 会损耗一些性能，渲染就会进行复制， 可以使用memo解决
+              React.cloneElement(button, {key: index})
+            )
+          }
         </footer>
-        
+      
       </div>
     </Fragment> : null
   )
+}
+Dialog.defaultProps ={
+  clickMaskClose: false
 }
 export default Dialog
